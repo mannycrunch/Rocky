12a71d6b7db6ccbbb603a1fcb20ab004de6fbb73
mannycrunch <75176130+mannycrunch@users.noreply.github.com>
2021-01-25 22:26:46 -0600

    Added Email

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -1,17 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Rocky.Data;
 using Rocky.Models;
+using Rocky.Models.ViewModels;
 using Rocky.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Rocky.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
 
         public CartController(ApplicationDbContext db)
         {
@@ -31,6 +38,55 @@ namespace Rocky.Controllers
             IEnumerable<Product> prodList = _db.Product.Where(p => prodInCart.Contains(p.Id));
 
             return View(prodList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            //var userId = User.FindFirstValue(ClaimTypes.Name);
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                // session exists
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(p => prodInCart.Contains(p.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value),
+                ProductList = prodList
+            };
+
+            return View(ProductUserVM);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                // session exists
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            shoppingCartList.Remove(shoppingCartList.FirstOrDefault(i => i.ProductId == id));
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ Rocky/Utility/EmailSender.cs
@@ -0,0 +1,63 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email, subject, htmlMessage);
+        }
+
+        public async Task Execute(string email, string subject, string body)
+        {
+            MailjetClient client = new MailjetClient("5e536c329eb2db680affbab9a06b2503", "29105d53414cae332f9df93194959c90")
+            {
+                //Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+                 new JObject {
+                      {
+                           "From",
+                           new JObject {
+                                {"Email", "manuel.lopez.jr@gmail.com"},
+                                {"Name", "Manny"}
+                           }
+                      }, {
+                           "To",
+                           new JArray {
+                                new JObject {
+                                     {
+                                          "Email",
+                                          email
+                                     }, {
+                                          "Name",
+                                          "DotNetMastery"
+                                     }
+                                }
+                           }
+                      }, {
+                           "Subject",
+                           subject
+                      }, {
+                           "HTMLPart",
+                           body
+                      }
+                 }
+             });
+            //MailjetResponse response = await client.PostAsync(request);
+            await client.PostAsync(request);
+        }
+    }
+}
